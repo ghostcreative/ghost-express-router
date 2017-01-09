c481bfa3295fb70820a672c8994d6d0759332a42
Nick Gerner <hello@njgerner.com>
2017-01-08 19:47:41 -0500

    ready for v1

--- test/test-GhostExpressRouter.js
+++ test/test-GhostExpressRouter.js
@@ -20,8 +20,12 @@ let request;
 let server;
 let seed;
 
-const routeHandler = (req, res, next) => {
+const routeHandler200 = (req, res) => {
   res.sendStatus(200);
+};
+
+const routeHandler202 = (req, res) => {
+  res.sendStatus(202);
 };
 
 describe('GhostExpressRouter', function () {
@@ -29,22 +33,21 @@ describe('GhostExpressRouter', function () {
   before(() => {
 
     Router.handle('GET', '/', {
-      'profileLimitedReadOnlyAccess': routeHandler,
-      'profileFullAccess': routeHandler
+      'profileLimitedReadOnlyAccess': routeHandler200,
+      'profileFullAccess': routeHandler202
     });
 
-    Router.handle('PUT', '/', routeHandler);
+    Router.handle('PUT', '/', routeHandler200);
 
     Router.handle('POST', '/', {
-      'profileFullAccess': routeHandler
+      'profileFullAccess': routeHandler200
     });
-
-    console.log("Router Stack", Router.getRouter().stack)
 
     return Promise.resolve()
     .then(() => GhostExpressServer.create(Config.get('server')))
     .then(_server_ => server = _server_)
     .then(() => server.useRouter('/profile', Router.getRouter()))
+    .then(() => server.useMiddleware('*', Router.ErrorHandler))
     .then(() => dbSetup.syncAll())
     .then(() => dbSetup.setupEntireAccount({ email: 'seed@gmail.com', password: 'secure123$' }))
     .tap(_seed_ => seed = _seed_)
@@ -72,6 +75,14 @@ describe('GhostExpressRouter', function () {
 
     afterEach(() => server.stop());
 
+    it('should return a 401 Unauthorized when a bearerTkn is not provided', () => {
+      return request.get('/profile')
+      .expect(401)
+      .expect(res => {
+        expect(res.body.message).to.be.equal('Missing authorization token.');
+      })
+    });
+
     it('should allow to define router with routeOptions and properly validate GET & POST requests', () => {
       return request.get('/profile')
       .set('Authorization', `Bearer ${seed.bearerTkn}`)
@@ -80,7 +91,6 @@ describe('GhostExpressRouter', function () {
     });
 
     it('should return 403 Forbidden when attempting to access a route without permission', () => {
-
       return request.post('/profile')
       .set('Authorization', `Bearer ${seed.bearerTkn}`)
       .expect(403)
